f025c946f639fe7887a497ad55596af49bd57291
Caio Henrique Pontes de Andrade <caio3d.andrade@gmail.com>
2023-01-30 04:25:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+using DesafioPOO.Models;
+
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia( numero: "123456", modelo: "N95", imei: "111111", memoria: 128 );
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("---------------------------------");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone( numero: "9876543", modelo: "13 pro max", imei: "222222", memoria: 256 );
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
